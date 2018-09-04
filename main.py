a02bf779a2250a64059c1ea6b7986476d8e3c3a2
runderwood <runderwood2016@gmail.com>
2018-09-03 22:00:34 -0500

    finished product

--- main.py
+++ main.py
@@ -9,25 +9,25 @@ form = """
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
         <form method = "POST">
             <p>Rotate By: <input type = 'text' name = 'rot' value = '0'/></p>
-            <p><textarea name = 'text' ></textarea></p>
+            <p><textarea name = 'text' >{0}</textarea></p>
             <p><input type = 'submit' name = 'Submit' value = 'Submit Query' /></p>
         </form>
     </body>
@@ -36,15 +36,14 @@ form = """
 
 @app.route("/")
 def index():
-    return form
+    return form.format("")
 
 @app.route("/", methods = ['POST'])
 def encrypt():
     rotateBy = int(request.form['rot'])
     toBeRotated = request.form['text']
-    rotate_string(toBeRotated, rotateBy)
-    toBeRotated = '<h1>{0}</h1>' . format(toBeRotated)
+    display = rotate_string(toBeRotated, rotateBy)
 
-    return toBeRotated
+    return form.format(display)
 
 app.run()
